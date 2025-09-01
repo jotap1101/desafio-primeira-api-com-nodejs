23ccdc8936888c33bbde167d61254de62fe6f449
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-08-31 22:47:45 -0300

    refactor: restructure Fastify Swagger registration for improved readability

--- server.ts
+++ server.ts
@@ -23,45 +23,47 @@ const server = fastify({
   },
 }).withTypeProvider<ZodTypeProvider>();
 
-server.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "Desafio Primeira API com Node.js",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non orci nibh. Fusce quis posuere turpis, nec imperdiet nunc. Aenean non ex sem. Ut aliquam elementum dui, consectetur euismod diam. Etiam lectus lacus, efficitur vel leo sed, venenatis posuere metus. Maecenas ut orci ullamcorper, malesuada urna eget, fermentum arcu. Morbi euismod est vitae mi tincidunt porttitor. Sed sodales vulputate ipsum, sed gravida nisl venenatis sed. Suspendisse convallis ante in leo semper ullamcorper. Donec dignissim nunc quis tortor finibus pellentesque.",
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: "http://localhost:3333",
-        description: "Development server",
+if (process.env.NODE_ENV === "development") {
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "Desafio Primeira API com Node.js",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non orci nibh. Fusce quis posuere turpis, nec imperdiet nunc. Aenean non ex sem. Ut aliquam elementum dui, consectetur euismod diam. Etiam lectus lacus, efficitur vel leo sed, venenatis posuere metus. Maecenas ut orci ullamcorper, malesuada urna eget, fermentum arcu. Morbi euismod est vitae mi tincidunt porttitor. Sed sodales vulputate ipsum, sed gravida nisl venenatis sed. Suspendisse convallis ante in leo semper ullamcorper. Donec dignissim nunc quis tortor finibus pellentesque.",
+        version: "1.0.0",
       },
-    ],
-    tags: [
-      {
-        name: "Courses",
-        description: "Operations related to courses",
-      },
-    ],
-    components: {
-      securitySchemes: {
-        apiKey: {
-          type: "apiKey",
-          name: "apiKey",
-          in: "header",
+      servers: [
+        {
+          url: "http://localhost:3333",
+          description: "Development server",
+        },
+      ],
+      tags: [
+        {
+          name: "Courses",
+          description: "Operations related to courses",
+        },
+      ],
+      components: {
+        securitySchemes: {
+          apiKey: {
+            type: "apiKey",
+            name: "apiKey",
+            in: "header",
+          },
         },
       },
+      externalDocs: {
+        url: "https://swagger.io",
+        description: "Find more info here",
+      },
     },
-    externalDocs: {
-      url: "https://swagger.io",
-      description: "Find more info here",
-    },
-  },
-  transform: jsonSchemaTransform,
-});
-server.register(scalarAPIReference, {
-  routePrefix: "/docs",
-});
+    transform: jsonSchemaTransform,
+  });
+  server.register(scalarAPIReference, {
+    routePrefix: "/docs",
+  });
+}
 
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
