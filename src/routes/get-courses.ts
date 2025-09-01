c3c43dd0a71f642afb2492143a98b70722b3ca3d
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-09-01 14:29:44 -0300

    feat: enhance getCoursesRoute with search, pagination, and ordering functionality

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,3 +1,4 @@
+import { and, asc, ilike, SQL } from "drizzle-orm";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { db } from "../db/index.ts";
@@ -11,6 +12,14 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         tags: ["Courses"],
         summary: "Get all courses",
         description: "This route retrieves all courses.",
+        querystring: z.object({
+          search: z
+            .string()
+            .optional()
+            .describe("Search term for course titles"),
+          orderBy: z.enum(["title"]).optional().default("title"),
+          page: z.coerce.number().optional().default(1),
+        }),
         response: {
           200: z
             .object({
@@ -21,15 +30,32 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                   description: z.string().nullable(),
                 })
               ),
+              total: z.number().min(0),
             })
             .describe("List of courses"),
         },
       },
     },
     async (request, reply) => {
-      const result = await db.select().from(coursesTable);
+      const { search, orderBy, page } = request.query;
+      const conditions: SQL[] = [];
 
-      return reply.status(200).send({ courses: result });
+      if (search) {
+        conditions.push(ilike(coursesTable.title, `%${search}%`));
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select()
+          .from(coursesTable)
+          .orderBy(asc(coursesTable[orderBy]))
+          .limit(5)
+          .offset((page - 1) * 5)
+          .where(and(...conditions)),
+        db.$count(coursesTable, and(...conditions)),
+      ]);
+
+      return reply.status(200).send({ courses: result, total });
     }
   );
 };
