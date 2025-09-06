563348510fb5cb7b53dcd8c43e142233df69f3d8
jotap1101 <jotap1101.joaopedro@gmail.com>
2025-09-05 23:13:22 -0300

    feat: add course seeding functionality and update enrollments to reference new courses

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -49,53 +49,88 @@ async function seed() {
     ])
     .returning();
 
-  const courses = await db.select().from(coursesTable);
+  console.log(usersInsert.map((user) => user.id));
+
+  const coursesInsert = await db
+    .insert(coursesTable)
+    .values([
+      {
+        title: "Curso de Node.js",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        title: "Curso de React.js",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        title: "Curso de Angular",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        title: "Curso de Vue.js",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+      {
+        title: "Curso de Svelte",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      },
+    ])
+    .returning();
+
+  console.log(coursesInsert.map((course) => course.id));
 
   const enrollmentsInsert = await db
     .insert(enrollmentsTable)
     .values([
       {
         userId: usersInsert[0].id,
-        courseId: courses[0].id,
+        courseId: coursesInsert[0].id,
       },
       {
         userId: usersInsert[1].id,
-        courseId: courses[0].id,
+        courseId: coursesInsert[0].id,
       },
       {
         userId: usersInsert[2].id,
-        courseId: courses[1].id,
+        courseId: coursesInsert[1].id,
       },
       {
         userId: usersInsert[3].id,
-        courseId: courses[1].id,
+        courseId: coursesInsert[1].id,
       },
       {
         userId: usersInsert[4].id,
-        courseId: courses[2].id,
+        courseId: coursesInsert[2].id,
       },
       {
         userId: usersInsert[5].id,
-        courseId: courses[2].id,
+        courseId: coursesInsert[2].id,
       },
       {
         userId: usersInsert[6].id,
-        courseId: courses[3].id,
+        courseId: coursesInsert[3].id,
       },
       {
         userId: usersInsert[7].id,
-        courseId: courses[3].id,
+        courseId: coursesInsert[3].id,
       },
       {
         userId: usersInsert[8].id,
-        courseId: courses[4].id,
+        courseId: coursesInsert[4].id,
       },
       {
         userId: usersInsert[9].id,
-        courseId: courses[4].id,
+        courseId: coursesInsert[4].id,
       },
     ])
     .returning();
+
+  console.log(enrollmentsInsert.map((enrollment) => enrollment.id));
 }
 
 seed();
